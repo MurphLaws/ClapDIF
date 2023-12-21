275b13b487d34f6f53421a199859ab5e3a535c6a
MurphLaws <lasso.nicolas@correounivalle.edu.co>
2023-12-21 13:19:00 +0100

    Modified influence implementation

--- populate.py
+++ populate.py
@@ -1,17 +1,48 @@
-
-
-#Import CIFAR-10 dataset using Pytoch without any transformation
-
 import torch
-import torchvision
-import torch.utils.data.dataset
+from captum.attr import visualization as viz
+from torchvision import models
+from captum.influence import TracInCPFast, TracInCP
+from Helpers.nicolas import DatasetLoaders, Resnet20Model
 
-#Download only 10% of the whole dataset, only two classes
+# Create a model (example: ResNet50)
+model = Resnet20Model(num_classes=10)
+model.eval()
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=None)
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=None)
+# Create a TracInCP attribute method
 
-#Create a dataloader for the training and test set
+#Define a random train dataset and dataloader
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=True, num_workers=2)
+train_data = torch.rand(100, 3, 224, 224)
+train_labels = torch.randint(0, 10, (100,))
+
+
+train_data = torch.utils.data.TensorDataset(train_data, train_labels)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+
+ckpt_path = 'poison_frog/model_saves/poisoned_model'
+checkpoint = torch.load(ckpt_path)
+pretrained = checkpoint["net"]
+
+#load the model on the path using torch
+
+tracincp = TracInCP(model, train_loader, [pretrained])
+
+# Load an input sample (you may need to preprocess your data accordingly)
+test_sample = torch.rand(1, 3, 224, 224)
+test_label = torch.randint(0, 10, (1,))
+
+#Make a dataloader for the single test sample
+
+test_sample = torch.utils.data.TensorDataset(test_sample, test_label)
+
+#Copmute the influece of input_tensor on the model
+
+#tracin_cp.influence((test_examples_features, test_examples_true_labels), show_progress=True)
+
+tracincp.influence((test_sample, test_label), show_progress=True)
+
+#load the checkpoint
+
+
+
+#attributions = tracincp.influence(test_sample)
